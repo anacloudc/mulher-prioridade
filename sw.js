f12fd3469c39910e15ffa5f88fa3ffaf2fc3a7bf
anacloudc <ana.cloudc@gmail.com>
2026-03-08 13:29:57 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,23 @@
+const CACHE_NAME = "mulher-prioridade-v1";
 
+const URLS_TO_CACHE = [
+  "/mulher-prioridade/",
+  "/mulher-prioridade/quiz.html",
+  "/mulher-prioridade/app.html",
+  "/mulher-prioridade/acesso.html",
+  "/mulher-prioridade/obrigada.html"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
